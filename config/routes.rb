42a9d7dff16cdc2477b89f18c62e1200a465ba98
brianryu <s.brianryu@gmail.com>
2019-04-17 16:26:37 -0400

    sounds controller & routes setup to include new&create sounds

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 namespace :api do
     namespace :v1 do
     	resources :drumkits, only: [:index, :update]
-    	resources :sounds, only: [:index, :update]
+    	resources :sounds, only: [:index, :new, :create, :update]
     end
   end
 
